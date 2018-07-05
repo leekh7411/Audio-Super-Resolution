9ed56c1ae73f2a0433d2d60f184865a024e24542
lee kwang ho <leekh7411@naver.com>
2018-07-05 15:16:39 +0900

    asr-net file update

--- asr_model.py
+++ asr_model.py
@@ -5,28 +5,17 @@ from scipy import interpolate
 from model import Model, default_opt
 from subpixel import SubPixel1D, SubPixel1D_v2
 from standard import conv1d, deconv1d
-from keras import backend as K
-from keras.layers import merge
-from keras.layers.core import Activation, Dropout
-from keras.layers.convolutional import Convolution1D
-from keras.layers.normalization import BatchNormalization
-from keras.layers.advanced_activations import LeakyReLU
-from keras.initializations import normal, orthogonal
 
-class ASRNet(Model): 
+class ASRNet(): 
     # based AudioUNet
-    """Generic tensorflow model training code"""
-
     def __init__(self, from_ckpt=False, n_dim=None, r=2,
                opt_params=default_opt, log_prefix='./run'):
         # perform the usual initialization
         # r : ?
         # n_dim : number of dimension
         self.r = r
-        Model.__init__(self, from_ckpt=from_ckpt, n_dim=n_dim, r=r,
-                       opt_params=opt_params, log_prefix=log_prefix)
-    
-    def create_model2(self, n_dim, r):
+           
+    def create_model(self, n_dim, r):
         # load inputs
         X, _, _ = self.inputs
         
@@ -36,10 +25,10 @@ class ASRNet(Model):
         with tf.name_scope('generator'):
             x = X
             L = self.layers
-            #n_filters = [128, 256,512,512, 512, 512, 512, 512]
-            n_filters = [12,  24,  48, 48, 48, 48, 48, 48]
-            #n_filtersizes = [65, 33, 17,  9,  9,  9,  9, 9, 9]
-            n_filtersizes = [10, 7, 5,  3,  3,  3,  3, 3, 3]
+            n_filters = [128, 256,512,512, 512, 512, 512, 512]
+            #n_filters = [12,  24,  48, 48, 48, 48, 48, 48]
+            n_filtersizes = [65, 33, 17,  9,  9,  9,  9, 9, 9]
+            #n_filtersizes = [10, 7, 5,  3,  3,  3,  3, 3, 3]
             downsampling_l = []
             
             print('Layers:',L)
@@ -62,19 +51,25 @@ class ASRNet(Model):
             # bottleneck layer
             #with tf.name_scope('bottleneck_conv'):
             x = tf.layers.conv1d(x,
-                                 filters = nf,
-                                 kernel_size = fs,
+                                 filters = n_filters[-1],
+                                 kernel_size = n_filtersizes[-1],
                                  strides=1,
                                  padding='same',
                                  activation=tf.nn.relu)
             x = tf.layers.dropout(x,rate=0.5,training=True) # TODO
             
+            print(nf, fs)
             #x = conv1d(x, n_filters[-1], n_filtersizes[-1], nl='prelu', dropOut=True)
             print('Bottle-Neck: ', x.get_shape())
 
             # upsampling layers
-            for l, nf, fs, l_in in zip(reversed(range(L)), reversed(n_filters), reversed(n_filtersizes), reversed(downsampling_l)):
+            L = reversed(range(L))
+            n_filters = reversed(n_filters)
+            n_filtersizes = reversed(n_filtersizes)
+            downsampling_l = reversed(downsampling_l)
+            for l, nf, fs, l_in in zip( L, (n_filters), (n_filtersizes), (downsampling_l)):
                 #with tf.name_scope('upsc_conv%d' % l):
+                
                 # (-1, n/2, 2f)
                 print('(-1, n/2, 2f)')
                 print(x.get_shape())
@@ -102,12 +97,14 @@ class ASRNet(Model):
                 print(l_in.get_shape())
                 
                 #x = tf.concat([x,l_in],axis=-1)
-                #x = tf.keras.layers.concatenate([x, l_in],axis=-1)
+                x = tf.keras.layers.concatenate([x, l_in],axis=-1)
+                
                 
                 print('U-Block: ', x.get_shape())
 
             # final conv layer
             #with tf.name_scope('lastconv'):
+           
             x = tf.layers.conv1d(x,
                                  filters = 2,
                                  kernel_size = 9,
@@ -118,77 +115,14 @@ class ASRNet(Model):
             print(x.get_shape())
             x = SubPixel1D(x, r=2) 
             print(x.get_shape())
-
+            
+            
             # add with original input
             #g = tf.add(x,X)
-            #g = tf.keras.layers.add([x, X])
+            g = tf.keras.layers.add([x, X])
             
         return x
     
-    def create_model(self, n_dim, r):
-        # load inputs
-        X, _, _ = self.inputs
-        K.set_session(self.sess)
-
-        with tf.name_scope('generator'):
-            x = X
-            L = self.layers
-            # dim/layer: 4096, 2048, 1024, 512, 256, 128,  64,  32,
-            # n_filters = [  64,  128,  256, 384, 384, 384, 384, 384]
-            n_filters = [  128,  256,  512, 512, 512, 512, 512, 512]
-            # n_filters = [  256,  512,  512, 512, 512, 1024, 1024, 1024]
-            # n_filtersizes = [129, 65,   33,  17,  9,  9,  9, 9]
-            # n_filtersizes = [31, 31,   31,  31,  31,  31,  31, 31]
-            n_filtersizes = [65, 33, 17,  9,  9,  9,  9, 9, 9]
-            downsampling_l = []
-
-            print ('building model...')
-
-            # downsampling layers
-            for l, nf, fs in zip(range(L), n_filters, n_filtersizes):
-                with tf.name_scope('downsc_conv%d' % l):
-                    x = (Convolution1D(nb_filter=nf, filter_length=fs, 
-                          activation=None, border_mode='same', init=orthogonal,
-                          subsample_length=2))(x)
-                    # if l > 0: x = BatchNormalization(mode=2)(x)
-                    x = LeakyReLU(0.2)(x)
-                    print ('D-Block: ', x.get_shape())
-                    downsampling_l.append(x)
-
-        # bottleneck layer
-        with tf.name_scope('bottleneck_conv'):
-            x = (Convolution1D(nb_filter=n_filters[-1], filter_length=n_filtersizes[-1], 
-                  activation=None, border_mode='same', init=orthogonal,
-                  subsample_length=2))(x)
-            x = Dropout(p=0.5)(x)
-            # x = BatchNormalization(mode=2)(x)
-            x = LeakyReLU(0.2)(x)
-
-        # upsampling layers
-        for l, nf, fs, l_in in zip(reversed(range(L)), reversed(n_filters), reversed(n_filtersizes), reversed(downsampling_l)):
-            with tf.name_scope('upsc_conv%d' % l):
-                # (-1, n/2, 2f)
-                x = (Convolution1D(nb_filter=2*nf, filter_length=fs, 
-                      activation=None, border_mode='same', init=orthogonal))(x)
-                # x = BatchNormalization(mode=2)(x)
-                x = Dropout(p=0.5)(x)
-                x = Activation('relu')(x)
-                # (-1, n, f)
-                x = SubPixel1D(x, r=2) 
-                # (-1, n, 2f)
-                x = merge([x, l_in], mode='concat', concat_axis=-1) 
-                print ('U-Block: ', x.get_shape())
-
-            # final conv layer
-            with tf.name_scope('lastconv'):
-                x = Convolution1D(nb_filter=2, filter_length=9, 
-                        activation=None, border_mode='same', init=normal)(x)    
-                x = SubPixel1D(x, r=2) 
-                print (x.get_shape())
-
-        g = merge([x, X], mode='sum')
-
-        return g
 
     def predict(self, X):
         assert len(X) == 1
--- standard.py
+++ standard.py
@@ -57,14 +57,14 @@ def conv1d(x, n_filters, n_size, stride=1, nl='relu', name='conv1d', dropOut=Fal
 
 def deconv1d(x, r, n_chan, n_in_dim, n_in_chan, name='deconv1d'):
     x = tf.reshape(x, [128, 1, n_in_dim, n_in_chan])
-    #with tf.variable_scope(name):
-    # filter : [height, width, output_channels, in_channels]
-    W = tf.get_variable('W', shape=[1, r, n_chan, n_in_chan],
-                        initializer=tf.random_normal_initializer(stddev=1e-3))
-    b = tf.get_variable('b', [n_chan], initializer=tf.constant_initializer(0.))
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+        # filter : [height, width, output_channels, in_channels]
+        W = tf.get_variable('W', shape=[1, r, n_chan, n_in_chan],
+                            initializer=tf.random_normal_initializer(stddev=1e-3))
+        b = tf.get_variable('b', [n_chan], initializer=tf.constant_initializer(0.))
 
-    x = tf.nn.conv2d_transpose(x, W, output_shape=(128, 1, r*n_in_dim, n_chan),
-                               strides=[1, 1, r, 1])
-    x = tf.nn.bias_add(x, b)
+        x = tf.nn.conv2d_transpose(x, W, output_shape=(128, 1, r*n_in_dim, n_chan),
+                                   strides=[1, 1, r, 1])
+        x = tf.nn.bias_add(x, b)
 
     return tf.reshape(x, [-1, r*n_in_dim, n_chan])
